f84dba27f859aec438abfc4cc05904a614764420
caraerskine <104041881+caraerskine@users.noreply.github.com>
2022-10-27 12:16:24 -0400

    added card styling via bootstrap

--- src/components/ContestantCard.js
+++ src/components/ContestantCard.js
@@ -1,25 +1,44 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 function ContestantCard( {champion} ) {
 
-    console.log(champion)
+
+    function handleClick() {
+        return (
+           <div>
+           <li>Winning word: <strong>{champion.word}</strong></li>
+           <li>Definition: <strong>{champion.definition}</strong></li>
+           </div>
+
+        )
+    }
     
     return (
-        <div className="row">
-                 <ul className="column">
-                 <h2>{champion.name}</h2>
-                    <img src={champion.image} alt={"Person holding trophy"} />
-                    <li>Championship: {champion.year}</li>
-                    <li>Hometown: {champion.location}</li>
-                    <li>Winning word: <strong>{champion.word}</strong></li>
-                    <li>Definition: <strong>{champion.definition}</strong></li>
-                     <p></p>
-                     <p></p>                   
-                 </ul>
-        </div>
-
-
-        );
+    <div>
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={champion.image} />
+                 <Card.Body>
+                     <Card.Title><strong><h2>{champion.name}</h2></strong></Card.Title>
+                         <Card.Text>
+                            <li>Championship: {champion.year}</li>
+                            <li>Hometown: {champion.location}</li>
+                        </Card.Text>
+                      <Button OnClick={handleClick} variant="primary">See{champion.name}'s winning word!</Button>
+                      <p></p>
+                      <p></p>
+                 </Card.Body>
+             </Card>      
+    </div>
+        
+      );
     }
 
     export default ContestantCard;
+
+
+    //need handle click on winning word button 
+
+    // <li>Winning word: <strong>{champion.word}</strong></li>
+    // <li>Definition: <strong>{champion.definition}</strong></li>
