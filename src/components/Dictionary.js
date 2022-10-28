33471783e97a7cf7c48b21fb5e20c83a6a8c6f28
caraerskine <104041881+caraerskine@users.noreply.github.com>
2022-10-28 16:33:52 -0400

    trying to fix p error

--- src/components/Dictionary.js
+++ src/components/Dictionary.js
@@ -4,10 +4,10 @@ import React from "react";
 function Dictionary( {champions} ) { 
 
     const renderContestants = champions.map((champion) => (
-     <div>
+     <>
         <p className="b">{champion.word}</p>
         <p className="c">{champion.definition}</p> 
-      </div>
+      </>
     ))
 
     return (
