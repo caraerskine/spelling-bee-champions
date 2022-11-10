4c9d335da93269534b1765067119a6c38b581245
caraerskine <104041881+caraerskine@users.noreply.github.com>
2022-11-10 14:49:56 -0500

    fixed broken jpg

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,13 @@ import React from "react";
 function Header() {
  return (
     <header>
-       <img src="https://i.postimg.cc/C1fyLNkp/bee-banner.png" alt="My BEESPN logo"/>
+       <img src="https://i.postimg.cc/wBq0zLGF/beespn-erskine-logo.jpg" alt="My BEESPN logo"/>
     </header>
  );
 }
 
 
 export default Header;
+
+
 
