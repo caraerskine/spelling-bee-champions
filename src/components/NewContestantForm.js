545d06cfae5bce6487b115476e35b51adfd4bc72
caraerskine <104041881+caraerskine@users.noreply.github.com>
2022-11-11 10:36:26 -0500

    clarify form

--- src/components/NewContestantForm.js
+++ src/components/NewContestantForm.js
@@ -1,11 +1,24 @@
 import React, { useState } from "react";
 
+//react makes it easy to manipulate data using forms and 
+//form input values can be set to state values and updated via react events.
+//defining a form's input value via state is considered a controlled component.
+//this is a controlled form it links a value to our state variable so state controls the form's data
+//making it easier for us to control the data
+//state is dynamic and powerful
+//anything in state is able to be manipulated
+//using state to interact with the input value
+
+//a controlled form 
+//state and setState aka state variables allow for flow. The flow "pushes" the value changes to the form
+//component so the form component always has the current value of the input. Data and UI are always in sync.
+
 //reset form
 //using state to create an object 
 //i am referring to line 18
 //when the form state is updated, the entire state gets rewritten
 
-const baseValue = {
+const initialState = {
   name: "",
   image: "",
   year: "",
@@ -15,32 +28,25 @@ const baseValue = {
 }
 
 function NewContestantForm( {setChampions} ) {
-  const [newContestant, setNewContestant] = useState(baseValue);
+  const [newContestant, setNewContestant] = useState(initialState);
 
   console.log(newContestant)
 
-
-  let value = 0
-   value++
-   console.log(value)
-
-   //forms 
-//this is a controlled form it links a value to our state variable so state controls the form's data
-//making it easier to control the data
-
+//upon rendering the page, useState sets the initial value of the form that I want filled out
+//and sets it according to what base value
+//newContestant is a variable
+//when it gets filled in with the value, that value becomes newContestant
 
 //with a static variable there is no re-render
-//state holds its value between the renders  
+//state holds its value between the renders which is why we use state in forms
+//that is what makes them controlled it is controlled by state
 
 //receiveing it from state
-//setting state as an obejct and 
-//callback funciton on it
+//setting state as an object and 
+//callback function on it
 //talk about the handle change
 //effective because we can give names to the inputs
 //thats what we can set the state with
-//state is dynamic and powerful
-//anything in state is able to be manipulated
-
 
 //handlechange is how we get controlled form, updates state to reflect what get put into the fields as user enters info
 //handle change happens when the event happens and the event is someone typing in
@@ -48,6 +54,12 @@ function NewContestantForm( {setChampions} ) {
 //take copy of of currentNC
 //target name rpesents the field and the target value reps what they put it the form field
 //the callback fn is passed into setNewContestant as an argument (currentNewContestant)
+
+//the setter function allows the progrmamer to cpature data and store it in a varibale 
+//calling it on line 62
+//return on line 66 the rest of the base value variables (currentNewContestant)
+//line 68 fill in name and value, what user is putting in
+//
 
 function handleChange(e) {
   setNewContestant((currentNewContestant) => {
@@ -63,16 +75,15 @@ function handleChange(e) {
   //
 }
 
-//app at top
 //passing set champions to add contesant form
 //when i add it i am setting champions
 
-//store it where it persists
-//submitting it to the API itself
-//submit to the post req of champions 
-
 //clicks the submit button when everyhitng the added to the fllds get ssubmitted
 
+//line 91 body: newContestant in my initial state passed into the JSON
+//stringify turns it into json on the back end so it can be received in proper format
+//turned into JSON
+//then on the second .then 
 
    function handleSubmit(e) {
     e.preventDefault();
@@ -92,7 +103,7 @@ function handleChange(e) {
 // on line 91 that is what is setting it to empty after I submit, clears it out to default state  
 
    function handleAddNewContestant(data) {
-     setNewContestant(baseValue) 
+     setNewContestant(initialState) 
      setChampions((currentContestants) => [...currentContestants, data])      
 }
 
@@ -103,6 +114,17 @@ function handleChange(e) {
 
 //git checkout main
 
+
+//Below is my form with several controlled inputs. The input is controlled because it I set its value via a prop.
+ // see Line 125 value={newContestant.name}
+
+ //About handleChange in my form. handleChange is called everytime a new character is typed in the input field. It takes in the new value of the input
+ //and sets it in state. It starts out as an empty string, you type "a" and handleChange takes in the event and calls setNewContestant
+ //taking in currentNewContestant as a variable and returns the currentNewContestant with all of its name and values that the user has put in.
+ 
+ //The handleSubmit is called on the form and gets the current values and adds them to the array when the submit button is pressed
+ //by the user has an onSubmit action which triggers the handleSubmit method. handleSubmit function above then takes in that event and makes a 
+ //POST request 
 
     return (
       <div className="new-contestant-form">
